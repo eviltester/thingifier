22922f3a158d230ea6c9a044eb7e8f83d01d7d7d
Alan Richardson <alan@compendiumdev.co.uk>
2018-07-15 23:02:17 +0100

    removed unnecessary code

--- src/main/java/uk/co/compendiumdev/thingifier/api/RoutingVerb.java
+++ src/main/java/uk/co/compendiumdev/thingifier/api/RoutingVerb.java
@@ -1,16 +1,6 @@
 package uk.co.compendiumdev.thingifier.api;
 
 public enum RoutingVerb {
-    GET("GET"), HEAD("HEAD"), DELETE("DELETE"), PATCH("PATCH"), PUT("PUT"), OPTIONS("OPTIONS"), POST("POST");
+    GET, HEAD, DELETE, PATCH, PUT, OPTIONS, POST;
 
-    private final String verbtext;
-
-    RoutingVerb(String verbtext) {
-        this.verbtext = verbtext;
-    }
-
-    @Override
-    public String toString() {
-        return this.verbtext;
-    }
 }
