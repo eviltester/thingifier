1ab09aead6b4c12edf5bf8d274d5ef7f7a79a90a
Alan Richardson <alan@compendiumdev.co.uk>
2018-07-25 19:46:21 +0100

    updated to 1.4 for continued development

--- src/main/java/uk/co/compendiumdev/thingifier/application/ThingifierVersionDetails.java
+++ src/main/java/uk/co/compendiumdev/thingifier/application/ThingifierVersionDetails.java
@@ -2,7 +2,7 @@ package uk.co.compendiumdev.thingifier.application;
 
 public class ThingifierVersionDetails {
 
-    final public static String VERSION_NUMBER = "1.3";
+    final public static String VERSION_NUMBER = "1.4";
 
     final public static String COPYRIGHT_YEAR = "2018";
 }
