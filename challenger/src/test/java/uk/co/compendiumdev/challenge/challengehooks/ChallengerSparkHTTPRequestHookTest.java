7cc8715d239d2b47aa5b4f77cfc0765ad4ef2834
alan <alan@compendiumdev.co.uk>
2020-08-19 13:01:18 +0100

    adding some mocking tests to demonstrate how to work with Spark in the current forms - really I should be refactoring the code to have HttpRequest and HttpResponse with a conversion abstraction, but this is a 'valid' excuse to explore mockito in more detail

--- challenger/src/test/java/uk/co/compendiumdev/challenge/challengehooks/ChallengerSparkHTTPRequestHookTest.java
+++ challenger/src/test/java/uk/co/compendiumdev/challenge/challengehooks/ChallengerSparkHTTPRequestHookTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import spark.Request;
 import spark.Response;
 import uk.co.compendiumdev.challenge.CHALLENGE;
+import uk.co.compendiumdev.challenge.ChallengerAuthData;
 import uk.co.compendiumdev.challenge.challengers.Challengers;
+
+import java.lang.reflect.Field;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -13,7 +16,7 @@ import static org.mockito.Mockito.when;
 public class ChallengerSparkHTTPRequestHookTest {
 
     @Test
-    public void canTriggerAHookTest(){
+    public void inMultUserModeWeNeedAnXChallengerHeaderToTrackChallenges(){
 
         Challengers challengers = new Challengers();
         challengers.setMultiPlayerMode();
@@ -35,7 +38,69 @@ public class ChallengerSparkHTTPRequestHookTest {
     }
 
     @Test
-    public void inSinglePlayerModeNoHeaderMeansDefaultChallenger(){
+    public void inMultUserModeAddingAnXChallengerHeaderWillTouchTheChallenger() throws NoSuchFieldException, IllegalAccessException {
+
+        Challengers challengers = new Challengers();
+        final ChallengerAuthData challenger = challengers.createNewChallenger();
+        long initialTouchTime = challenger.getLastAccessed();
+
+        // risk that this test is intermittent if it all happens in the same millisecond
+        // if so could 'wait' in the test, or 'hack the object to have a different last accessed time'
+        // set private variable that test will 'touch'
+//        Field lastAccessedField = ChallengerAuthData.class.getDeclaredField("lastAccessed");
+//        lastAccessedField.setAccessible(true);
+//        lastAccessedField.set(challenger, 0L);
+//        Assertions.assertEquals(0, challenger.getLastAccessed());
+
+        challengers.setMultiPlayerMode();
+
+        long preTouchTime = System.currentTimeMillis();
+
+        final ChallengerSparkHTTPRequestHook hook = new ChallengerSparkHTTPRequestHook(challengers);
+
+        Request request = mock(Request.class);
+        Response response = mock(Response.class);
+        when(request.headers("X-CHALLENGER")).thenReturn(challenger.getXChallenger());
+        when(request.requestMethod()).thenReturn("GET");
+        when(request.pathInfo()).thenReturn("/challenges");
+        hook.run(request, response);
+
+        // nothing changes - assert on this
+        // we passed the GET /challenges for default user
+        Assertions.assertTrue(
+                challenger.statusOfChallenge
+                        (CHALLENGE.GET_CHALLENGES));
+
+        Assertions.assertNotEquals(0, challenger.getLastAccessed());
+        Assertions.assertNotEquals(initialTouchTime, challenger.getLastAccessed());
+        Assertions.assertTrue(challenger.getLastAccessed() >= preTouchTime);
+    }
+
+
+    @Test
+    public void inMultUserModeWeNeedAnXChallengerHeaderThatExistsToTrackChallenges(){
+
+        Challengers challengers = new Challengers();
+        challengers.setMultiPlayerMode();
+
+        final ChallengerSparkHTTPRequestHook hook = new ChallengerSparkHTTPRequestHook(challengers);
+
+        Request request = mock(Request.class);
+        Response response = mock(Response.class);
+        when(request.headers("X-CHALLENGER")).thenReturn("bobobobobobobobobobo");
+        when(request.requestMethod()).thenReturn("GET");
+        when(request.pathInfo()).thenReturn("/challenges");
+        hook.run(request, response);
+
+        // nothing changes - assert on this
+        // we passed the GET /challenges for default user
+        Assertions.assertFalse(
+                challengers.SINGLE_PLAYER.statusOfChallenge
+                        (CHALLENGE.GET_CHALLENGES));
+    }
+
+    @Test
+    public void inSinglePlayerModeNoHeaderMeansUseDefaultChallenger(){
 
         Challengers challengers = new Challengers();
 
--- thingifier/src/main/java/uk/co/compendiumdev/thingifier/application/sparkhttpmessageHooks/SparkRequestResponseHook.java
+++ thingifier/src/main/java/uk/co/compendiumdev/thingifier/application/sparkhttpmessageHooks/SparkRequestResponseHook.java
@@ -4,6 +4,6 @@ import spark.Request;
 import spark.Response;
 
 public interface SparkRequestResponseHook {
-    // throw an exception if we want to 'stop' the request
+    // throw an exception if we want to 'stop' the request and return the response
     void run(Request request, Response response);
 }
