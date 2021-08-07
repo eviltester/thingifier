c24d1a9630f21ef91bb7b4bf6cb2e0e7cf226440
alan <alan@compendiumdev.co.uk>
2021-08-07 15:05:25 +0100

    get challenges now uses a thingifier to respond to xml or json headers

---
+++ challenger/src/main/java/uk/co/compendiumdev/challenge/apimodel/ChallengeThingifier.java
@@ -0,0 +1,63 @@
+package uk.co.compendiumdev.challenge.apimodel;
+
+import uk.co.compendiumdev.challenge.ChallengerAuthData;
+import uk.co.compendiumdev.challenge.challenges.ChallengeDefinitionData;
+import uk.co.compendiumdev.challenge.challenges.ChallengeDefinitions;
+import uk.co.compendiumdev.thingifier.Thingifier;
+import uk.co.compendiumdev.thingifier.core.Thing;
+import uk.co.compendiumdev.thingifier.core.domain.definitions.field.definition.Field;
+import uk.co.compendiumdev.thingifier.core.domain.definitions.field.definition.FieldType;
+import uk.co.compendiumdev.thingifier.core.domain.definitions.validation.MaximumLengthValidationRule;
+import uk.co.compendiumdev.thingifier.core.domain.instances.ThingInstance;
+
+public class ChallengeThingifier {
+
+    public Thingifier challengeThingifier;
+    public Thing challengeDefn;
+    private ChallengeDefinitions challengeDefinitions;
+
+    public ChallengeThingifier(){
+        createThingifier();
+    }
+
+    private void createThingifier(){
+        // fake the data storage
+        this.challengeThingifier = new Thingifier();
+
+        this.challengeThingifier.apiConfig().setResponsesToShowGuids(false);
+
+        this.challengeDefn = this.challengeThingifier.createThing(
+                                    "challenge", "challenges");
+
+        this.challengeDefn.definition().addFields(
+                Field.is("id", FieldType.ID),
+                Field.is("name", FieldType.STRING).withDefaultValue(""),
+                Field.is("description", FieldType.STRING).withDefaultValue(""),
+                Field.is("status", FieldType.BOOLEAN).withDefaultValue("false")
+        );
+    }
+
+    public void populateThingifierFrom(ChallengeDefinitions challengeDefinitions) {
+        this.challengeDefinitions = challengeDefinitions;
+        // create all instances from the definitions, then when we want to
+        // set all the status codes to the specific challenger status
+        for (ChallengeDefinitionData challenge : challengeDefinitions.getChallenges()) {
+            this.challengeDefn.createManagedInstance().
+                    overrideValue("id", challenge.id).
+                    setValue("name", challenge.name).
+                    setValue("description", challenge.description);
+        }
+    }
+
+    public void populateThingifierFromStatus(ChallengerAuthData challenger){
+        for (ChallengeDefinitionData challenge : challengeDefinitions.getChallenges()) {
+            final ThingInstance instance = this.challengeDefn.findInstanceByGUIDorID(challenge.id);
+            instance.setValue("status",
+                                        challenger.statusOfChallenge(
+                                                challengeDefinitions.
+                                                        getChallenge(challenge.name)
+                                ).toString()
+            );
+        }
+    }
+}
--- challenger/src/main/java/uk/co/compendiumdev/challenge/challengesrouting/ChallengesRoutes.java
+++ challenger/src/main/java/uk/co/compendiumdev/challenge/challengesrouting/ChallengesRoutes.java
@@ -2,25 +2,37 @@ package uk.co.compendiumdev.challenge.challengesrouting;
 
 import uk.co.compendiumdev.challenge.ChallengerAuthData;
 import uk.co.compendiumdev.challenge.ChallengesPayload;
+import uk.co.compendiumdev.challenge.apimodel.ChallengeThingifier;
 import uk.co.compendiumdev.challenge.challengers.Challengers;
 import uk.co.compendiumdev.challenge.challenges.ChallengeDefinitions;
 import uk.co.compendiumdev.thingifier.api.ThingifierApiDefn;
+import uk.co.compendiumdev.thingifier.api.http.HttpApiRequest;
+import uk.co.compendiumdev.thingifier.api.http.HttpApiResponse;
+import uk.co.compendiumdev.thingifier.api.http.ThingifierHttpApi;
+import uk.co.compendiumdev.thingifier.api.response.ApiResponse;
 import uk.co.compendiumdev.thingifier.api.routings.RoutingDefinition;
 import uk.co.compendiumdev.thingifier.api.routings.RoutingStatus;
 import uk.co.compendiumdev.thingifier.api.routings.RoutingVerb;
+import uk.co.compendiumdev.thingifier.application.internalhttpconversion.HttpApiResponseToSpark;
+import uk.co.compendiumdev.thingifier.application.internalhttpconversion.SparkToHttpApiRequest;
+import uk.co.compendiumdev.thingifier.reporting.JsonThing;
 import uk.co.compendiumdev.thingifier.spark.SimpleRouteConfig;
+
+import java.util.ArrayList;
 
 import static spark.Spark.*;
 
 public class ChallengesRoutes {
 
     // TODO: challenges should responds to request headers and allow conversion into XML or JSON
+    // todo: allow filtering challenges e.g. find all done, not done, etc.
     public void configure(final Challengers challengers, final boolean single_player_mode,
                           final ThingifierApiDefn apiDefn,
                           final ChallengeDefinitions challengeDefinitions){
+
         get("/challenges", (request, result) -> {
-            result.status(200);
-            result.type("application/json");
+//            result.status(200);
+//            result.type("application/json");
 
             ChallengerAuthData challenger = challengers.getChallenger(request.headers("X-CHALLENGER"));
 
@@ -32,11 +44,40 @@ public class ChallengesRoutes {
                 result.raw().setHeader("Location", "/gui/challenges");
             }
 
-            result.body(new ChallengesPayload(challengeDefinitions, challenger).getAsJson());
-            return "";
+//            result.body(new ChallengesPayload(challengeDefinitions, challenger).getAsJson());
+//
+            final HttpApiRequest myRequest = SparkToHttpApiRequest.convert(request);
+
+            HttpApiResponse httpApiResponse=null;
+
+            ChallengeThingifier challengeThingifier = new ChallengeThingifier();
+            challengeThingifier.populateThingifierFrom(challengeDefinitions);
+
+            final ThingifierHttpApi httpApi = new ThingifierHttpApi(challengeThingifier.challengeThingifier);
+            final JsonThing jsonThing = new JsonThing(challengeThingifier.challengeThingifier.apiConfig().jsonOutput());
+
+            httpApiResponse = httpApi.validateRequestSyntax(myRequest,
+                                ThingifierHttpApi.HttpVerb.GET);
+
+            if (httpApiResponse == null) {
+                challengeThingifier.populateThingifierFromStatus(challenger);
+                final ApiResponse apiResponse = ApiResponse.success().
+                                                    returnInstanceCollection(
+                                                            new ArrayList(
+                                                                    challengeThingifier.
+                                                                            challengeDefn.getInstances())
+                                                    );
+
+                httpApiResponse = new HttpApiResponse(myRequest.getHeaders(), apiResponse,
+                        jsonThing, challengeThingifier.challengeThingifier.apiConfig());
+            }
+
+            return HttpApiResponseToSpark.convert(httpApiResponse, result);
+
+            //return "";
         });
 
-        // todo: allow filtering challenges e.g. find all done, not done, etc.
+
 
         head("/challenges", (request, result) -> {
             result.status(200);
