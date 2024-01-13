0368ce731fcc7a72e3341576d1aa8d3eff99c82c
Alan Richardson <alan@compendiumdev.co.uk>
2024-01-13 12:46:13 +0000

    added capability for multiple databases with a singleton interface over a default

--- ercoremodel/src/main/java/uk/co/compendiumdev/thingifier/core/EntityRelModel.java
+++ ercoremodel/src/main/java/uk/co/compendiumdev/thingifier/core/EntityRelModel.java
@@ -10,7 +10,9 @@ import uk.co.compendiumdev.thingifier.core.domain.instances.EntityInstance;
 import uk.co.compendiumdev.thingifier.core.domain.instances.EntityInstanceCollection;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
     The ERM has the 'model' (ERSchema) and the 'instances' (things).
@@ -19,24 +21,30 @@ import java.util.List;
  */
 public class EntityRelModel {
 
+    private final String DEFAULT_DATABASE_NAME = "__default";
+
     // TODO: this should be a Map so that key, database can be used
     // e.g. key from a 'session', or 'custom' or 'default'
-    private final ERInstanceData database;
+    private final Map<String, ERInstanceData> databases;
     private final ERSchema schema; // all the definitions
 
     public EntityRelModel(){
         schema = new ERSchema();
-        database = new ERInstanceData();
+        databases = new HashMap<String,ERInstanceData>();
+        databases.put(DEFAULT_DATABASE_NAME, new ERInstanceData());
     }
 
     public EntityRelModel(final ERSchema schema, final ERInstanceData erInstanceData) {
         this.schema = schema;
-        this.database = erInstanceData;
+        this.databases = new HashMap<String, ERInstanceData>();
+        this.databases.put(DEFAULT_DATABASE_NAME,erInstanceData);
     }
 
     public EntityDefinition createEntityDefinition(final String entityName, final String pluralName) {
         EntityDefinition defn = schema.defineEntity(entityName, pluralName);
-        database.createInstanceCollectionFor(defn);
+        for(ERInstanceData database : databases.values()){
+            database.createInstanceCollectionFor(defn);
+        }
         return defn;
     }
 
@@ -45,7 +53,12 @@ public class EntityRelModel {
     }
 
     public ERInstanceData getInstanceData(){
-        return database;
+        return databases.get(DEFAULT_DATABASE_NAME);
+    }
+
+    // Object Level
+    public EntityRelModel cloneWithDifferentData(final List<EntityInstance> instances) {
+        return new EntityRelModel(schema, new ERInstanceData(instances));
     }
 
     // Schema methods
@@ -85,26 +98,24 @@ public class EntityRelModel {
     // Instance Methods
 
     public List<EntityInstanceCollection> getAllEntityInstanceCollections() {
-        return database.getAllInstanceCollections();
+        return getInstanceData().getAllInstanceCollections();
     }
 
     public EntityInstance findEntityInstanceByGuid(final String thingGUID) {
-        return database.findEntityInstanceByGUID(thingGUID);
+        return getInstanceData().findEntityInstanceByGUID(thingGUID);
     }
 
     public EntityInstanceCollection getInstanceCollectionForEntityNamed(final String aName) {
-        return database.getInstanceCollectionForEntityNamed(aName);
+        return getInstanceData().getInstanceCollectionForEntityNamed(aName);
     }
 
     public void deleteEntityInstance(final EntityInstance anEntityInstance) {
-        database.deleteEntityInstance(anEntityInstance);
+        getInstanceData().deleteEntityInstance(anEntityInstance);
     }
 
     public void clearAllData() {
-        database.clearAllData();
+        getInstanceData().clearAllData();
     }
 
-    public EntityRelModel cloneWithDifferentData(final List<EntityInstance> instances) {
-        return new EntityRelModel(schema, new ERInstanceData(instances));
-    }
+
 }
