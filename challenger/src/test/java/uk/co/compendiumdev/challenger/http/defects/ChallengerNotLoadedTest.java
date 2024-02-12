948c57f7b2e0769a9a4cae333188aa25183d1cab
Alan Richardson <alan@compendiumdev.co.uk>
2024-02-12 18:51:03 +0000

    create folder for test

--- challenger/src/test/java/uk/co/compendiumdev/challenger/http/defects/ChallengerNotLoadedTest.java
+++ challenger/src/test/java/uk/co/compendiumdev/challenger/http/defects/ChallengerNotLoadedTest.java
@@ -20,6 +20,7 @@ public class ChallengerNotLoadedTest{
         String notLoadedChallengerGUID = "2ce954c6-caa1-4299-85af-205a9d9f7867";
         final File resourceFile = new File(System.getProperty("user.dir") , "/src/test/resources/" + notLoadedChallengerGUID + ".data.txt");
         final File folder = new File(System.getProperty("user.dir"), "challengersessions");
+        folder.mkdirs();
         final File dataFile = new File(folder , notLoadedChallengerGUID + ".data.txt");
 
         if(!resourceFile.exists()){
