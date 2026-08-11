16940e1cfb02168f957f6dc44d078cfbd3fad9bb
Alan Richardson <alan@compendiumdev.co.uk>
2026-08-11 11:10:58 +0100

    Apply response views to JSONPath queries

--- thingifier/src/main/java/uk/co/compendiumdev/thingifier/api/restapihandlers/RestApiQueryHandler.java
+++ thingifier/src/main/java/uk/co/compendiumdev/thingifier/api/restapihandlers/RestApiQueryHandler.java
@@ -11,11 +11,14 @@ import uk.co.compendiumdev.thingifier.adapter.http.apihandlers.route.Relationshi
 import uk.co.compendiumdev.thingifier.adapter.http.apihandlers.route.RelationshipInstanceRoute;
 import uk.co.compendiumdev.thingifier.adapter.http.apihandlers.route.ThingRoute;
 import uk.co.compendiumdev.thingifier.adapter.http.apihandlers.route.ThingRouteMapper;
+import uk.co.compendiumdev.thingifier.api.docgen.RoutingVerb;
 import uk.co.compendiumdev.thingifier.api.ermodelconversion.JsonThing;
 import uk.co.compendiumdev.thingifier.api.http.ApiRequestEnvelope;
 import uk.co.compendiumdev.thingifier.api.http.ThingifierHttpApi;
 import uk.co.compendiumdev.thingifier.api.http.ThingifierRequestContext;
 import uk.co.compendiumdev.thingifier.api.response.ApiResponse;
+import uk.co.compendiumdev.thingifier.core.domain.definitions.EntityDefinition;
+import uk.co.compendiumdev.thingifier.core.domain.definitions.EntityViewDefinition;
 import uk.co.compendiumdev.thingifier.core.query.QueryFilterParams;
 import uk.co.compendiumdev.thingifier.core.query.RepositoryQueryResult;
 
@@ -79,9 +82,10 @@ public final class RestApiQueryHandler {
         }
 
         if (queryBodyFormat == ApiRequestEnvelope.QueryBodyFormat.JSONPATH) {
+            final EntityViewDefinition responseView = responseViewFor(url, response);
             response =
                     new JsonPathQueryFilter(new JsonThing(runtime.apiConfig().jsonOutput()))
-                            .filter(queryResults, context, queryBody);
+                            .filter(queryResults, context, queryBody, responseView);
         }
 
         return advertiseQuery(response);
@@ -89,6 +93,23 @@ public final class RestApiQueryHandler {
 
     private ApiResponse methodNotAllowed(final String allowHeader) {
         return ApiResponse.error(405, "Method Not Allowed").setHeader("Allow", allowHeader);
+    }
+
+    private EntityViewDefinition responseViewFor(final String url, final ApiResponse response) {
+        if (response == null
+                || response.isErrorResponse()
+                || response.hasABodyOverride()
+                || response.getTypeOfThingReturned() == null) {
+            return null;
+        }
+
+        final EntityDefinition entity = response.getTypeOfThingReturned();
+        return runtime.apiSpec()
+                .ruleFor(RoutingVerb.QUERY, url, runtime.apiConfig().getApiEndPointPrefix())
+                .map(rule -> rule.responseEntityViewFor(response.getStatusCode()))
+                .filter(entity::hasViewNamed)
+                .map(entity::getViewNamed)
+                .orElse(null);
     }
 
     private ApiResponse advertiseQuery(final ApiResponse response) {
